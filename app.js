38e6a8d37d4d0b211d51efc58aec3439cc949e13
parkchangeon <qkrcksrjs@gmail.com>
2022-06-06 21:51:06 +0900

    read state + tailwindcss

--- app.js
+++ app.js
@@ -6,32 +6,50 @@ const content = document.createElement("div")
 
 const store = {
   currentPage: 1,
-  maxLength: 0,
+  feeds: [],
 }
 
 function getData(url) {
   ajax.open("GET", url, false)
   ajax.send()
+
   return JSON.parse(ajax.response)
 }
 
-function newsFeed() {
-  const newsFeed = getData(NEWS_URL)
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i += 1) feeds[i].read = false
 
-  store.maxLength = newsFeed.length
+  store.feeds = feeds
+  store.maxLength = feeds.length
+}
+
+function newsFeed() {
+  if (store.feeds.length === 0) makeFeeds(getData(NEWS_URL))
 
   let template = `
-  <div class="container mx-auto p-4">
-    <h1>Hacker News</h1>
-    <ul>
-      {{__news_feed__}}
-    </ul>
-    <div>
-      <a href="#/page/{{__prev_page__}}">이전 페이지</a>
-      <a href="#/page/{{__next_page__}}">다음 페이지</a>
+  <div class="bg-gray-600 min-h-screen">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+              Previous
+            </a>
+            <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+              Next
+            </a>
+         </div>
+        </div> 
+      </div>
+    </div>
+    <div class="p-4 text-2xl text-gray-700">
+     {{__news_feed__}}        
     </div>
   </div>
-  `
+`
 
   const newsList = []
   for (
@@ -40,11 +58,27 @@ function newsFeed() {
     i += 1
   ) {
     newsList.push(`
-  <li>
-    <a href="#/show/${newsFeed[i].id}">
-      ${newsFeed[i].title} - (${newsFeed[i].comments_count})
-    </a>
-  </li>
+    <div class="p-6 ${
+      store.feeds[i].read ? "bg-red-500" : "bg-white"
+    } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+    <div class="flex">
+      <div class="flex-auto">
+        <a href="#/show/${store.feeds[i].id}">${store.feeds[i].title}</a>  
+      </div>
+      <div class="text-center text-sm">
+        <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
+          store.feeds[i].comments_count
+        }</div>
+      </div>
+    </div>
+    <div class="flex mt-3">
+      <div class="grid grid-cols-3 text-sm text-gray-500">
+        <div><i class="fas fa-user mr-1"></i>${store.feeds[i].user}</div>
+        <div><i class="fas fa-heart mr-1"></i>${store.feeds[i].points}</div>
+        <div><i class="far fa-clock mr-1"></i>${store.feeds[i].time_ago}</div>
+      </div>  
+    </div>
+  </div>
   `)
   }
 
@@ -55,7 +89,7 @@ function newsFeed() {
   )
   template = template.replace(
     "{{__next_page__}}",
-    Math.ceil(store.maxLength / 10) === store.currentPage
+    Math.ceil(store.feeds.length / 10) === store.currentPage
       ? store.currentPage
       : store.currentPage + 1
   )
@@ -63,18 +97,75 @@ function newsFeed() {
   container.innerHTML = template
 }
 
+function checkRead(id) {
+  for (let i = 0; i < store.feeds.length; i += 1) {
+    if (store.feeds[i].id === id) {
+      store.feeds[i].read = true
+      break
+    }
+  }
+}
+
 function newsDetail() {
   const id = location.hash.substring(7)
-
+  checkRead(Number(id))
   const newsContent = getData(CONTENT_URL.replace("@id", id))
 
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
     </div>
   `
+
+  function makeComment(comments, deps = 0) {
+    const commentString = []
+
+    for (let i = 0; i < comments.length; i++) {
+      commentString.push(`
+        <div style="padding-left: ${deps * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `)
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, deps + 1))
+      }
+    }
+
+    return commentString.join("")
+  }
+
+  container.innerHTML = template.replace(
+    "{{__comments__}}",
+    makeComment(newsContent.comments)
+  )
 }
 
 function router() {
