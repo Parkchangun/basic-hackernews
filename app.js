717db8ddd02ffabd9f7512b6d77684c45bdf1369
parkchangeon <qkrcksrjs@gmail.com>
2022-06-06 17:54:55 +0900

    DOM API -> 문자열 innerHTML / router 기능 추가

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
 const ajax = new XMLHttpRequest()
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"
-
 const container = document.getElementById("root")
 const content = document.createElement("div")
 
-ajax.open("GET", NEWS_URL, false)
-ajax.send()
-const newsFeed = JSON.parse(ajax.response)
-
-const ul = document.createElement("ul")
-
-for (let i = 0; i < 10; i += 1) {
-  const li = document.createElement("li")
-  const a = document.createElement("a")
-
-  a.href = `#${newsFeed[i].id}`
-  a.textContent += `${newsFeed[i].title} - (${newsFeed[i].comments_count})`
-
-  li.appendChild(a)
-  ul.appendChild(li)
-}
-container.appendChild(ul)
-
-window.addEventListener("hashchange", function () {
-  const id = this.location.hash.substring(1)
-
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false)
+function getData(url) {
+  ajax.open("GET", url, false)
   ajax.send()
-  const newsContent = JSON.parse(ajax.response)
+  return JSON.parse(ajax.response)
+}
 
-  const title = this.document.createElement("h1")
-  title.textContent += `${newsContent.title}`
-  content.append(title)
-})
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL)
 
-container.appendChild(content)
+  const newsList = []
+
+  newsList.push("<ul>")
+  for (let i = 0; i < 10; i += 1) {
+    newsList.push(`
+  <li>
+    <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} - (${newsFeed[i].comments_count})
+    </a>
+  </li>
+  `)
+  }
+  newsList.push("</ul>")
+
+  container.innerHTML = newsList.join("")
+}
+
+function newsDetail() {
+  const id = location.hash.substring(1)
+
+  const newsContent = getData(CONTENT_URL.replace("@id", id))
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `
+}
+
+function router() {
+  const routePath = location.hash
+
+  if (routePath === "") newsFeed()
+  else newsDetail()
+}
+
+window.addEventListener("hashchange", router)
+
+router()
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,31 +123,39 @@ var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 var container = document.getElementById("root");
 var content = document.createElement("div");
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
-var ul = document.createElement("ul");
 
-for (var i = 0; i < 10; i += 1) {
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  a.href = "#".concat(newsFeed[i].id);
-  a.textContent += "".concat(newsFeed[i].title, " - (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
 }
 
-container.appendChild(ul);
-window.addEventListener("hashchange", function () {
-  var id = this.location.hash.substring(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
-  var title = this.document.createElement("h1");
-  title.textContent += "".concat(newsContent.title);
-  content.append(title);
-});
-container.appendChild(content);
+function newsFeed() {
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push("<ul>");
+
+  for (var i = 0; i < 10; i += 1) {
+    newsList.push("\n  <li>\n    <a href=\"#".concat(newsFeed[i].id, "\">\n      ").concat(newsFeed[i].title, " - (").concat(newsFeed[i].comments_count, ")\n    </a>\n  </li>\n  "));
+  }
+
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
+  var id = location.hash.substring(1);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
+}
+
+function router() {
+  var routePath = location.hash;
+  if (routePath === "") newsFeed();else newsDetail();
+}
+
+window.addEventListener("hashchange", router);
+router();
 },{}],"../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -176,7 +184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57773" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53476" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
